bd83ce151ac92bbbb5853402aa969345fa5cb569
Ginters <GB664@live.mdx.ac.uk>
2021-03-24 19:56:46 +0000

    Update with README, local

--- src/Park.java
+++ src/Park.java
@@ -3,11 +3,10 @@ public class Park extends Venue {
     private int numChangingFacilities = 5;
 
     public Park() {
-        name = "London";
+
     }
 
     public void display() {
-        System.out.println("Name of park: " + name);
-        System.out.println("Number of changing facilities: " + numChangingFacilities);
+
     }
 }
